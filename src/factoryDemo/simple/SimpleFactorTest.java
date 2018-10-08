a2a3fb287d3652cc9514246ec8551e48d480f85c
jun.zhang <jun.zhang@fleety.com>
2018-10-08 11:34:24 +0800

    test

--- src/factoryDemo/simple/SimpleFactorTest.java
+++ src/factoryDemo/simple/SimpleFactorTest.java
@@ -6,6 +6,7 @@ public class SimpleFactorTest {
 		// TODO Auto-generated method stub
 		SimpleFactory factory = new SimpleFactory();
 		System.out.println(factory.getName("Haiwei"));
+		System.out.println(factory.getName("Vivo"));
 
 	}
 
